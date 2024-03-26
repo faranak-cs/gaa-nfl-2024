7509065df72cf90f953cc28ee5ca8069a947969e
Faran Ahmad <fakhan.cs@gmail.com>
2024-03-26 03:16:14 +0000

    ng init

---
+++ a2ng_/src/app/teams.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Team } from './team';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class TeamsService {  
+
+  constructor()
+  {
+  }
+
+  // getTeams here
+
+}
