e99aba87c75f113b3bfa39bfb79ef31bed076680
Faran Ahmad <fakhan.cs@gmail.com>
2024-03-26 12:00:18 +0000

    teams page init

--- a2ng_/src/app/teams/teams.component.ts
+++ a2ng_/src/app/teams/teams.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Team } from '../team';
+import { TeamsService } from '../teams.service';
 
 @Component({
   selector: 'app-teams',
   standalone: true,
   imports: [],
   templateUrl: './teams.component.html',
-  styleUrl: './teams.component.css'
+  styleUrl: './teams.component.css',
 })
 export class TeamsComponent {
+  // teamURL: string = `assets/logos/${team.name}.png`;
+  teams: Team[] = [];
 
+  // Inject team service
+  teamService = inject(TeamsService);
+
+  constructor() {
+    this.teamService.getTeams().subscribe((res) => {
+      this.teams = res;
+    });
+  }
 }
