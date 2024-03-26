c9c5e274669254753019dfbdc7d70247912b4cfc
Faran Ahmad <fakhan.cs@gmail.com>
2024-03-26 05:44:22 +0000

    api

--- a2restapi_/index.js
+++ a2restapi_/index.js
@@ -1,25 +1,40 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var cors = require("cors");
+//
+var model = require("./model/db.js"); //
 
-var model = require('./model/db.js');  //
-
-var cors = require('cors')
 var app = express();
-
+app.use(cors());
 
 // serves files in public folder
-app.use(express.static('public'));
-app.use(cors());
+app.use(express.static("public"));
+
+// NB:: this must be included to get JSON content sent with requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+///////////////////////////////////////////////////////////////////////////////////////////
 
 //
 // routes
 //
 
 // /teams
+app.route("/teams/").get(function (req, res) {
+  model.getTeams(req, res);
+});
 
 // /players
+app.route("/players/").get(function (req, res) {
+  model.getPlayers(req, res);
+});
 
-  
-var myServer = app.listen(3000, function() {
+// /results
+app.route("/results/").get(function (req, res) {
+  model.getResults(req, res);
+});
+
+var myServer = app.listen(3000, function () {
   console.log("Server listening on port 3000");
 });
--- a2restapi_/model/db.js
+++ a2restapi_/model/db.js
@@ -1,4 +1,4 @@
-var mysql = require('mysql');
+var mysql = require("mysql");
 
 ///////////////////////////////////////////////////////////////////////////////////////////
 
@@ -6,16 +6,46 @@ var mysql = require('mysql');
 // timezone is very NB
 
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'gaanfl2024',
-  timezone: 'utc+0'  
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "gaanfl2024",
+  timezone: "utc+0",
 });
 
-connection.connect(function(err){
-	if(err) throw err;
-	console.log(`Sucessfully connected to MySQL database gaaNFL2024`);
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log(`Sucessfully connected to MySQL database gaaNFL2024`);
 });
 
 ///////////////////////////////////////////////////////////////////////////////////////////
+
+// GET /teams
+exports.getTeams = function (req, res) {
+  connection.query(`SELECT * from teams`, function (err, rows, fields) {
+    if (err) throw err;
+
+    res.status(200); // OK
+    res.send(JSON.stringify(rows));
+  });
+};
+
+// GET /players
+exports.getPlayers = function (req, res) {
+  connection.query(`SELECT * from players`, function (err, rows, fields) {
+    if (err) throw err;
+
+    res.status(200); // OK
+    res.send(JSON.stringify(rows));
+  });
+};
+
+// GET /results
+exports.getResults = function (req, res) {
+  connection.query(`SELECT * from results`, function (err, rows, fields) {
+    if (err) throw err;
+
+    res.status(200); // OK
+    res.send(JSON.stringify(rows));
+  });
+};
