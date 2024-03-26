3a8be33d5568d67301ac28b8706771b2fc2a2eae
Faran Ahmad <fakhan.cs@gmail.com>
2024-03-26 18:07:54 +0000

    results page init

--- a2ng_/src/app/results/results.component.ts
+++ a2ng_/src/app/results/results.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Result } from '../result';
+import { ResultsService } from '../results.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-results',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './results.component.html',
-  styleUrl: './results.component.css'
+  styleUrl: './results.component.css',
 })
 export class ResultsComponent {
+  results: Result[] = [];
 
+  resultService = inject(ResultsService);
+
+  constructor() {
+    this.resultService.getResults().subscribe((res) => {
+      this.results = res;
+    });
+  }
 }
