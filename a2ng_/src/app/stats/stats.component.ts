180bbd3d2af47ae1ad31b2e9b17948e84be63a55
Faran Ahmad <fakhan.cs@gmail.com>
2024-04-15 23:25:11 +0100

    Stats init

--- a2ng_/src/app/stats/stats.component.ts
+++ a2ng_/src/app/stats/stats.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { Team } from '../team';
+import { TeamsService } from '../teams.service';
 
 @Component({
   selector: 'app-stats',
   standalone: true,
   imports: [],
   templateUrl: './stats.component.html',
-  styleUrl: './stats.component.css'
+  styleUrl: './stats.component.css',
 })
 export class StatsComponent {
+  teams: Team[] = [];
+  team: string = 'Team';
 
+  // Inject services
+  constructor(private teamService: TeamsService) {}
+
+  // Get data
+  ngOnInit() {
+    this.teamService.getTeams().subscribe((res) => {
+      this.teams = res;
+    });
+  }
+
+  // Handlers
+
+  selectTeamHandler(event: Event) {
+    this.team = (event.target as HTMLSelectElement).value;
+    console.log(this.team);
+  }
 }
