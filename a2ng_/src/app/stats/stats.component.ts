70dbdcb34ffd1148744633106c5c68e7e5608dfe
Faran Ahmad <fakhan.cs@gmail.com>
2024-04-18 20:02:50 +0100

    Stats ✅

--- a2ng_/src/app/stats/stats.component.ts
+++ a2ng_/src/app/stats/stats.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Team } from '../team';
 import { TeamsService } from '../teams.service';
 import * as d3 from 'd3';
+import { ResultsService } from '../results.service';
+import { Result } from '../result';
 
 @Component({
   selector: 'app-stats',
@@ -11,18 +13,38 @@ import * as d3 from 'd3';
   styleUrl: './stats.component.css',
 })
 export class StatsComponent {
+  results: Result[] = [];
+  filterResults: Result[] = [];
+
   teams: Team[] = [];
   team: string = 'Team';
 
+  points: { x1: number; x2: number }[] = [
+    { x1: 0, x2: 30 },
+    { x1: 140, x2: 170 },
+    { x1: 280, x2: 310 },
+    { x1: 420, x2: 450 },
+  ];
+
   // Inject services
-  constructor(private teamService: TeamsService) {}
+  constructor(
+    private teamService: TeamsService,
+    private resultService: ResultsService
+  ) {}
 
   // Get data
   ngOnInit() {
+    this.resultService.getResults().subscribe((res) => {
+      this.results = res;
+
+      // Eliminate result with zero scores
+      this.results = this.results.filter(
+        (result) => result.team1Score !== '0' && result.team2Score !== '0'
+      );
+    });
+
     this.teamService.getTeams().subscribe((res) => {
       this.teams = res;
-
-      // d3 logic
     });
   }
 
@@ -30,6 +52,11 @@ export class StatsComponent {
 
   selectTeamHandler(event: Event) {
     this.team = (event.target as HTMLSelectElement).value;
-    console.log(this.team);
+
+    this.filterResults = this.team
+      ? this.results.filter(
+          (result) => result.team1 === this.team || result.team2 === this.team
+        )
+      : this.results;
   }
 }
